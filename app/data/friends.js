27c431e30179ed5fe4aba1b53a638538c1425b95
Abi D <axa.abigail.i@gmail.com>
2019-08-23 23:17:05 -0500

    changed index.html messages, populated friends.js w/ friends object array

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,4 +1,62 @@
 /* FRIENDS DATA */
 
-// create variable friendList as an empty array to store objects
-var friendList = [];
+// create variable compatible as an empty array to store objects of compatible friends
+var compatible = [];
+// create array of potential friends
+var friends = [
+    {
+        name: 'Pikachu',
+        photo: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png',
+        scores: [4,3,2,2,2,1,1,4,2,3]
+    },
+    {
+        name: 'Charmander',
+        photo: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png',
+        scores: [1,2,4,4,2,3,2,1,1,4]
+    },
+    {
+        name: 'Gengar',
+        photo: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/094.png',
+        scores: [2,3,2,1,4,5,3,5,1]
+    },
+    {
+        name: 'Greninja',
+        photo: 'https://cdn.bulbagarden.net/upload/thumb/6/67/658Greninja.png/1200px-658Greninja.png',
+        scores: [1,2,3,3,4,5,1,2,3,1]
+    },
+    {
+        name: 'Ditto',
+        photo: 'https://cdn.bulbagarden.net/upload/3/36/132Ditto.png',
+        scores: [5,5,5,5,5,5,5,5,5,5]
+    },
+    {
+        name: 'Eevee',
+        photo: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/133.png',
+        scores: [5,3,2,1,1,2,4,1,1,3]
+    },
+    {
+        name: 'Typhlosion',
+        photo: 'https://img.pokemondb.net/artwork/large/typhlosion.jpg',
+        scores: [3,1,3,4,1,4,1,4,2,3]
+    },
+    {
+        name: 'Mudkip',
+        photo: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/258.png',
+        scores: [2,1,4,4,2,1,5,2,5,2]
+    },
+    {
+        name: 'Mimikyu',
+        photo: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/778.png',
+        scores: [2,2,3,5,1,2,2,5,3,1]
+    },
+    {
+        name: 'Magikarp',
+        photo: 'https://cdn.bulbagarden.net/upload/0/02/129Magikarp.png',
+        scores: [2,2,1,1,1,2,4,2,1,5]
+    },
+    {
+        name: 'Pumpkaboo',
+        photo: 'https://cdn.bulbagarden.net/upload/d/df/710Pumpkaboo.png',
+        scores: [2,2,1,3,4,1,4,1,3,2]
+    }
+];
