cc989ae98dceecce560377eb2f690dbc7998618a
Abi D <axa.abigail.i@gmail.com>
2019-08-24 22:15:19 -0500

    added more logic for score comparison

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -37,6 +37,25 @@ module.exports = function(app) {
             userData.push(user);
         }); 
         res.json(true);
+
+        // compare userData to friends
+        function showFriend (){
+            // empty array of compared scores
+            var comparisonArray = [];
+            // easier way to refer to the latest user added to userData object array
+            var currentUser = userData[userData.length-1]
+            // for loop to loop through the friends array
+            for (let j=0; j < friends.length; j++) {
+                // for loop to loop through scores key of particular iteration of friends object array
+                for (let i=0; i < friends[j].scores.length; i++) {
+                    // defines index value in comparisonArray as specified score iteration of the specified friend object in the friends array 
+                    comparisonArray[i] = friend[j].scores[i] - currentUser.scores[i]
+                    // swaps sign of number to assure it is positive if negative
+                    if (Math.sign(comparisonArray[i]) === -1) {
+                        comparisonArray[i] = comparisonArray[i]*-1
+                }
+            }
+        }
     
     });
 }
