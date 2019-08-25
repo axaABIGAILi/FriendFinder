8f3326650625cd7e491d01f3c4dfae3b4a5734d9
Abi D <axa.abigail.i@gmail.com>
2019-08-24 23:05:07 -0500

    polished html script, cleaned up code on apiRoutes.js

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -16,39 +16,20 @@ module.exports = function(app) {
 
     // post to handle incoming data
     app.post('/api/userData', function(req, res){
-        /* CODE TO HANDLE INCOMING DATA AND POST TO DATABASE + HANDLE COMPATABILITY LOGIC*/
-   
-        // user object constructor
-        function userInfo (n, img, arr) {
-            this.name = n;
-            this.image = img;
-            this.scores = [arr]
-        };
-        // survey onclick event
-        $('#surveySub').on('click', function(){
-            var n = $('#name').val();
-            var img = $('#photo').val();
-            var arr = [];
-            for (var i=1; i <= 10; i++) {
-                arr.push($('#q'+i).val());
-            }
-            var user = new userInfo (n, img, arr);
-            // push the request body into the userData array
-            userData.push(user);
-        }); 
+        /* CODE TO HANDLE INCOMING DATA AND POST TO DATABASE + HANDLE COMPATABILITY LOGIC*/        
 
+        // totalDiff high value default for comparison updating purposes
         var totalDiff = 1000;
 
-        // compare userData to friends
-        function calculateFriend (){
-            // variables to store name and image
-            var friendName = '';
-            var friendImg = '' ;           
-            // easier way to refer to the latest user added to userData object array
-            var currentUser = userData[userData.length-1]
+        // variables to store name and image
+        var friendName = '';
+        var friendImg = '' ;   
+
+        // easier way to refer to the latest user added to userData object array
+        var currentUser = userData[userData.length-1]
             // for loop to loop through the friends array
-            for (let j=0; j < friends.length; j++) {
-                var difference = 0;
+        for (let j=0; j < friends.length; j++) {
+            var difference = 0;
                 // for loop to loop through scores key of particular iteration of friends object array
                 for (let i=0; i < friends[j].scores.length; i++) {
                     // defines index value in comparisonArray as specified score iteration of the specified friend object in the friends array 
@@ -61,7 +42,6 @@ module.exports = function(app) {
                     friendImg = friends.friends[i].photo;
                 };
             }
-        }
     res.json(true);
-    }
+    })
 }
