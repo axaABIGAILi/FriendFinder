58f3f9255c4a2b105616c9e233df55c243f3e40f
Abi D <axa.abigail.i@gmail.com>
2019-08-24 21:55:45 -0500

    added handling logic for user inputs and started ajax call in html

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -15,10 +15,29 @@ module.exports = function(app) {
     });
 
     // post to handle incoming data
-    app.post('/api/friends', function(req, res){
+    app.post('/api/userData', function(req, res){
         /* CODE TO HANDLE INCOMING DATA AND POST TO DATABASE + HANDLE COMPATABILITY LOGIC*/
-
-
+   
+        // user object constructor
+        function userInfo (n, img, arr) {
+            this.name = n;
+            this.image = img;
+            this.scores = [arr]
+        };
+        // survey onclick event
+        $('#surveySub').on('click', function(){
+            var n = $('#name').val();
+            var img = $('#photo').val();
+            var arr = [];
+            for (var i=1; i <= 10; i++) {
+                arr.push($('#q'+i).val());
+            }
+            var user = new userInfo (n, img, arr);
+            // push the request body into the userData array
+            userData.push(user);
+        }); 
+        res.json(true);
+    
     });
 }
 
